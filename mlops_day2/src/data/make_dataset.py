c56e2c8b1e349db98e3ebe630ea90c6ca5c9c4c9
Jesper A <jalkestrup@gmail.com>
2023-01-03 15:17:18 +0100

    Added predict model and train

--- mlops_day2/src/data/make_dataset.py
+++ mlops_day2/src/data/make_dataset.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
+import torch
+import numpy as np
 import click
 import logging
 from pathlib import Path
@@ -15,6 +18,24 @@ def main(input_filepath, output_filepath):
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
 
+    for tt in ['train','test']:
+
+        content = [np.load(f'{input_filepath}/{path}') for path in os.listdir(input_filepath) if tt in path]
+        images = [data['images'] for data in content]
+        labels = [data['labels'] for data in content]
+        images, labels = np.concatenate(images), np.concatenate(labels).reshape(-1,1)
+
+        # To tensor
+        images = torch.from_numpy(images.astype(np.float32)) 
+        labels = torch.from_numpy(labels.astype(np.float32)).type(torch.LongTensor)
+
+        # Normalize
+        mean, std = torch.mean(images), torch.std(images)
+        images = (images-mean)/std
+
+        # Save
+        torch.save(images, f'{output_filepath}/{tt}_images.pt')
+        torch.save(labels, f'{output_filepath}/{tt}_labels.pt')
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
